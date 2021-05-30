6728cbad11ca869d085197342e04c91935fda189
Samuele de Tomasi <el3um4s@gmail.com>
2021-05-30 09:41:52 +0200

    test svelte kit

---
+++ src/routes/about.svelte
@@ -0,0 +1,9 @@
+<h1>ABOUT</h1>
+
+<p> <a href="./">home</a> </p>
+
+<style>
+    h1 {
+        color: greenyellow;
+    }
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,13 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="./about">kit.svelte.dev</a> to read the documentation</p>
+
+
+<style>
+    h1 {
+        color: red;
+    }
+    p {
+        background-color: blue;
+        color: yellow;
+    }
+</style>
